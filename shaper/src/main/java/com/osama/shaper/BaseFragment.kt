477339ee62bb6bd0ebd9fdbfd2fcdf2bd0c3344f
Osama Raddad <osama.s.raddad@gmail.com>
2018-07-04 16:07:30 +0300

    fix

--- shaper/src/main/java/com/osama/shaper/BaseFragment.kt
+++ shaper/src/main/java/com/osama/shaper/BaseFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class BaseFragment : Fragment() {
+open class BaseFragment : Fragment() {
     protected lateinit var fragmentComponentManager: FragmentComponentManager<*>
         private set
 
